308a7ee7338a26f9ce923ec9c7a638d8a4f048c2
Taylor Friesen <taylorfriesen@protonmail.com>
2021-04-23 10:25:09 -0700

    authorization changes done

--- src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
+++ src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
@@ -59,6 +59,8 @@ public class ResourceServerConfig
                 .authenticated()
                 .antMatchers("/roles/**")
                 .hasAnyRole("ADMIN", "DATA")
+                .antMatchers("/books/**")
+                .hasAnyRole("ADMIN", "DATA", "USER")
                 .anyRequest().denyAll()
                 .and()
                 .exceptionHandling()
